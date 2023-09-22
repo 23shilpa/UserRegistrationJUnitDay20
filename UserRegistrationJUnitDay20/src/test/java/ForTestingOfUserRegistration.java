068a1dc8eb40d25954351f1e6b8b7272f88ead64
shilpa <shilpabhonde000@gmail.com>
2023-09-22 14:17:07 +0530

    UC11 Complete.

--- UserRegistrationJUnitDay20/src/test/java/ForTestingOfUserRegistration.java
+++ UserRegistrationJUnitDay20/src/test/java/ForTestingOfUserRegistration.java
@@ -12,5 +12,14 @@ public class ForTestingOfUserRegistration {
         Assert.assertEquals("Valid",UserRegistrationTesting.getUserEmailID("shilpa123.shilpa@co.co.in"));
         Assert.assertEquals("Valid",UserRegistrationTesting.getUserPhoneNumber("918888879983"));
         Assert.assertEquals("Valid",UserRegistrationTesting.getUserPassword("Doremon@1234"));
+        Assert.assertEquals("Valid",UserRegistrationTesting.getUserEmailID("abc@yahoo.com"));
+        Assert.assertEquals("Valid",UserRegistrationTesting.getUserEmailID("abc-100@yahoo.com"));
+        Assert.assertEquals("Valid",UserRegistrationTesting.getUserEmailID("abc.100@yahoo.com"));
+        Assert.assertEquals("Valid",UserRegistrationTesting.getUserEmailID("abc111@abc.com"));
+        Assert.assertEquals("Valid",UserRegistrationTesting.getUserEmailID("abc-100@abc.net"));
+        Assert.assertEquals("Valid",UserRegistrationTesting.getUserEmailID("abc.100@abc.com.au"));
+        Assert.assertEquals("Valid",UserRegistrationTesting.getUserEmailID("abc@1.com"));
+        Assert.assertEquals("Valid",UserRegistrationTesting.getUserEmailID("abc@gmail.com"));
+        Assert.assertEquals("Valid",UserRegistrationTesting.getUserEmailID("abc+100@gmail.com"));
     }
 }
